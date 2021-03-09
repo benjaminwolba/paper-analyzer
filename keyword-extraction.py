26e3a0f17a2fdeaf59b211f9589e1b87d2ea9914
Benjamin Wolba <mail@benjaminwolba.com>
2021-03-09 16:22:36 +0100

    TF-IDF Analysis

--- keyword-extraction.py
+++ keyword-extraction.py
@@ -1,9 +1,16 @@
 
+""" 
+NLP Analysis of Scientific Abstracts based on N-Grams Frequency and TF-IDF
+
+https://medium.com/analytics-vidhya/automated-keyword-extraction-from-articles-using-nlp-bfd864f41b34
+"""
+
 # =============================================================================
 # Import of Packages
 # =============================================================================
 
 import pandas
+from scipy.sparse import coo_matrix
 from pprint import pprint 
 
 import re
@@ -12,7 +19,9 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
+
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 from os import path
 from PIL import Image
@@ -122,10 +131,11 @@ plt.show()
 
 # Tokenisation & Vectorisation 
 
-# cv=CountVectorizer(max_df=0.8,stop_words=stop_words, max_features=10000, ngram_range=(1,3))
-# X=cv.fit_transform(corpus)
+cv=CountVectorizer(max_df=0.8,stop_words=stop_words, max_features=10000, ngram_range=(1,3))
+X=cv.fit_transform(corpus)
 # pprint(list(cv.vocabulary_.keys())[:10])
 
+"""
 # Most frequently occuring words
 def get_top_n_words(corpus, n=None):
     # vec = CountVectorizer().fit(corpus)
@@ -155,3 +165,61 @@ plt.tight_layout()
 
 fig = g.get_figure()
 fig.savefig('three-grams.png') 
+#"""
+
+# =============================================================================
+# TF-IDF
+# =============================================================================
+ 
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+tfidf_transformer.fit(X)
+
+# get feature names
+feature_names=cv.get_feature_names()
+ 
+# fetch document for which keywords needs to be extracted
+doc=corpus[12]
+ 
+# generate tf-idf for the given document
+tf_idf_vector=tfidf_transformer.transform(cv.transform([doc]))
+
+# Function for sorting tf_idf in descending order
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+ 
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """get the feature names and tf-idf score of top n items"""
+    
+    #use only topn items from vector
+    sorted_items = sorted_items[:topn]
+ 
+    score_vals = []
+    feature_vals = []
+    
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+ 
+    #create a tuples of feature,score
+    #results = zip(feature_vals,score_vals)
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+    
+    return results
+#sort the tf-idf vectors by descending order of scores
+sorted_items=sort_coo(tf_idf_vector.tocoo())
+
+#extract only the top n; n here is 10
+keywords=extract_topn_from_vector(feature_names,sorted_items,5)
+ 
+# now print the results
+print("\nAbstract:")
+print(doc)
+print("\nKeywords:")
+for k in keywords:
+    print(k,keywords[k])
